6f8a2eeedb67948294d13ef0143143d5e922bd17
Klimovich <diana_klimovich@bk.ru>
2023-08-11 13:43:40 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 module.exports = function toReadable(number) {
-  var a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
-  var b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const numwords = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+  ];
 
-  if ((number = number.toString()).length > 9) return 'overflow';
-   num = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-  if (!num) return; var str = '';
-   str += (num[0] == 0) ? (a[Number(num[0])] || b[num[0][0]] + ' ' + a[num[0][0]]) + 'zero' : '';
-   str += (num[1] != 0) ? (a[Number(num[1])] || b[num[1][0]] + ' ' + a[num[1][1]]) + 'crore ' : '';
-   str += (num[2] != 0) ? (a[Number(num[2])] || b[num[2][0]] + ' ' + a[num[2][1]]) + 'lakh ' : '';
-   str += (num[3] != 0) ? (a[Number(num[3])] || b[num[3][0]] + ' ' + a[num[3][1]]) + 'thousand ' : '';
-   str += (num[4] != 0) ? (a[Number(num[4])] || b[num[4][0]] + ' ' + a[num[4][1]]) + 'hundred ' : '';
-   str += (num[5] != 0) ? ((str != '') ? '' : '') + (a[Number(num[5])] || b[num[5][0]] + ' ' + a[num[5][1]]) : '';
-  return str.trim();
+  const tens = [
+    "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+  ];
+
+  if (number < 20) {
+    return numwords[number];
+  }
+
+  if (number >= 20 && number < 100) {
+    const tensDigit = Math.floor(number / 10);
+    const unitsDigit = number % 10;
+    if (unitsDigit === 0) {
+      return tens[tensDigit];
+    } else {
+      return tens[tensDigit] + " " + numwords[unitsDigit];
+    }
+  }
+
+  if (number >= 100) {
+    const hundredsDigit = Math.floor(number / 100);
+    const tensDigit = Math.floor((number % 100) / 10);
+    const unitsDigit = number % 10;
+    if (tensDigit === 0 && unitsDigit === 0) {
+      return numwords[hundredsDigit] + " hundred";
+    } else if (tensDigit === 0) {
+      return numwords[hundredsDigit] + " hundred " + numwords[unitsDigit];
+    } else if (tensDigit === 1) {
+      return numwords[hundredsDigit] + " hundred " + numwords[number % 100];
+    } else if (unitsDigit === 0) {
+      return numwords[hundredsDigit] + " hundred " + tens[tensDigit];
+    } else {
+      return numwords[hundredsDigit] + " hundred " + tens[tensDigit] + " " + numwords[unitsDigit];
+    }
+  }
 }
-
